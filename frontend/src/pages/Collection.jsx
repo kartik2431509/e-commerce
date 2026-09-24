d0dbc4a3d5fc18a3bc1b7098f991d971e0437718
kartik2431509 <kartik.2431509@gmail.com>
2026-09-24 15:07:42 +0530

    filter box

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,15 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import {ShopContext} from '../context/ShopContext';
 
 const Collection = () => {
 
   const {products} = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false)
 
   return (
-    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-1'>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
       {/* filter options */}
-      <div >
+      <div className='min-w-60'>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+
+        {/* Category Filter */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden' } sm:block`}>
+          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2'>
+              <input className='w-3' type='checkbox' value={'Men'}/> Men
+            </p>
+
+          </div>
+        </div>
 
       </div>
       
